e908f58bf8b8bbadb7540820858c2cd5c0295ce6
dharam011 <dharam801799@gmail.com>
2025-09-16 16:38:14 +0530

    axios thoda sa pdha h

--- Recipe manage/src/pages/Home.jsx
+++ Recipe manage/src/pages/Home.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { axios  } from '../utils/axios';
+
 
 const Home = () => {
+
+
+  const getProduct = async () =>  {
+    console.log("clicked")
+try {
+   const response = await fetch("/products")
+ const jsondata = await response.json()
+ console.log(response)
+ console.log(jsondata)
+// const { data }  = await axios.get("https://fakestoreapi.com/products/1")
+// console.log(data)
+} 
+  catch(error){
+    console.log(error);
+    
+  }
+  }
+
+
   return (
-    <div className="home w-full h-screen">Home</div>
+    <div className="home w-full h-screen">
+      <h1>Home</h1>
+      <button onClick={getProduct}>get products</button>
+      </div>
   )
 }
 
