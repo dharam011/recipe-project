f58d6bab34a438bc8d57ba6668c40df88fa8fa17
dharam011 <dharam801799@gmail.com>
2025-09-16 22:38:48 +0530

    fiexd asios issues

--- Recipe manage/src/pages/Home.jsx
+++ Recipe manage/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { axios  } from '../utils/axios';
+import Axios from '../utils/Axios';
 
 
 const Home = () => {
@@ -8,20 +8,18 @@ const Home = () => {
   const getProduct = async () =>  {
     console.log("clicked")
 try {
-   const response = await fetch("/products")
- const jsondata = await response.json()
- console.log(response)
- console.log(jsondata)
-// const { data }  = await axios.get("https://fakestoreapi.com/products/1")
-// console.log(data)
+//    const response = await fetch("/products")
+//  const jsondata = await response.json()
+//  console.log(response)
+//  console.log(jsondata)
+const { data }  = await Axios.get("https://fakestoreapi.com/products/1")
+console.log(data)
 } 
   catch(error){
     console.log(error);
     
   }
   }
-
-
   return (
     <div className="home w-full h-screen">
       <h1>Home</h1>
--- Recipe manage/src/utils/axios.jsx
+++ Recipe manage/src/utils/axios.jsx
@@ -1,8 +1,8 @@
-import { axios } from "axios";
+import  axios  from "axios";
 
 const instance = axios.create({
     baseURL : "https://fakestoreapi.com/",
-    withCredentials : true,
+    // withCredentials : true,
 }
 )
 instance.interceptors.request.use(function (config) {
